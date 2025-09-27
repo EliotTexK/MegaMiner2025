58b6e5cdc901405e4a1d3a9435f765eb5b8992fc
Mason Harbord <pugsiamese@gmail.com>
2025-09-27 18:18:06 -0500

    updated variables to reflect rules

--- backend/Constants.py
+++ backend/Constants.py
@@ -2,13 +2,11 @@ from GameObjects import *
 
 # BALANCE: tweaking these numbers will make or break game balance
 
-INITIAL_MONEY: int = 3
-HOUSE_MONEY_PRODUCED: int = 1
+INITIAL_MONEY: int = 20
+HOUSE_MONEY_PRODUCED: int = 12
 DEMON_INITIAL_HP: int = 70
 DEMON_ATTACK_POWER: int = 10 # TODO: increase this over time?
 MERCENARY_INITIAL_HP: int = 70
 MERCENARY_ATTACK_POWER: int = 10
 DEMON_SPAWNER_RELOAD_TURNS: int = 10
-BUILDER_PRICES: list = [4,8,16,32,64] # BALANCE: exploration/exploitation tradeoff, don't let players snowball too quickly
-MAX_BUILDERS = len(BUILDER_PRICES) + 1
 PLAYER_BASE_INITIAL_HP = 200
