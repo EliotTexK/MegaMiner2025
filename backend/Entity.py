5186e21ae8bbd6c55ab1de12f543d8b914ae0298
Eliot <eaknxp@umsystem.edu>
2025-09-27 17:55:48 -0500

    progess on towers

--- backend/Entity.py
+++ backend/Entity.py
@@ -1,7 +1,5 @@
 import Constants
 
-# Probably going to store health and position
-
 class Entity:
     def __int__(self, health: int, x: int, y: int) -> None:
         self.x = x
--- backend/Tower.py
+++ backend/Tower.py
@@ -1,11 +1,7 @@
-import Constants
-import math
-
-class Tower:
-    def __init__(self, x: int, y: int, tower_type: TowerType, team_color: str) -> None:
+class Tower(Entity):
+    def __init__(self, x: int, y: int, team_color: str) -> None:
         self.x = x
         self.y = y
-        self.tower_type = tower_type
         # BALANCE: initial reload formula: encourange long-term planning, but not too much
         # BALANCE: don't let the initial reload time be zero
         #self.reload_turns_left = math.ceil(self.tower_type.reload_turns//2)
@@ -15,11 +11,5 @@ class Tower:
         else:
             raise Exception("Mercenary team_color must be 'r' or 'b'") # TF2 reference?
 
-    def __update__(self, tower_type: TowerType) -> None:
-        # still working on this part, doesn't really work yet
-
-        #self.tower_type = tower_type
-        
-        #if tower_type == 'house':
-        #    print("HOUSE GENERATED MONEY")
-        pass
+    def __update__(self) -> None:
+        pass # Override this
