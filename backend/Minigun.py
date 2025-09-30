52ce451ea8fc50516643b8aa6400941279cd7584
Mason Harbord <pugsiamese@gmail.com>
2025-09-30 18:42:50 -0500

    changed terminology from 'gold' to 'money'

--- backend/Minigun.py
+++ backend/Minigun.py
@@ -3,4 +3,4 @@ from Tower import Tower
 
 class Minigun(Tower):
     def __init__(self, x: int, y: int, team_color: str) -> None:
-        super().__init__(self, x, y, team_color, constants.MINIGUN_MAX_COOLDOWN)
+        super().__init__(self, x, y, team_color, Constants.MINIGUN_MAX_COOLDOWN)
