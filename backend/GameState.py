512456613e7e6966507f742a9349f93606387b05
Eliot <eaknxp@umsystem.edu>
2025-09-30 17:31:04 -0500

    added bastard dfs algorithm for computing the merc path

--- backend/GameState.py
+++ backend/GameState.py
@@ -29,9 +29,36 @@ class GameState:
         self.demon_spawners = []
 
         # Compute mercenary paths, from Red to Blue player bases
-        self.mercenary_path_left  = [] # start from left-adjacent tile to player base
-        self.mercenary_path_right = [] # start from left-adjacent tile to player base
-        self.mercenary_path_up    = [] # start from left-adjacent tile to player base
-        self.mercenary_path_down  = [] # start from left-adjacent tile to player base
+        self.mercenary_path_left  = self.compute_mercenary_path((player_base_r.x-1, player_base_r.y))
+        self.mercenary_path_right = self.compute_mercenary_path((player_base_r.x+1, player_base_r.y))
+        self.mercenary_path_up    = self.compute_mercenary_path((player_base_r.x, player_base_r.y-1))
+        self.mercenary_path_down  = self.compute_mercenary_path((player_base_r.x, player_base_r.y+1))
 
-        if self.tile_grid[][]
+    def compute_mercenary_path(self, start_point: tuple) -> list:
+        # See if there's a path starting at the starting point
+        if self.tile_grid[start_point.x][start_point.y] == 'Path':
+            # Do bastard DFS algorithm: raise exception if there's any branch in the path
+            computed_path = [start_point]
+            current_tile = start_point
+            traversed = set()
+            traversed.add(start_point)
+
+            # Loop through new neighboring tiles until there are none left or a branch is detected
+            while current_tile != None:
+                # Find the next tile in the path
+                for neighbor in [
+                    (current_tile.x - 1, current_tile.y)
+                    (current_tile.x + 1, current_tile.y)
+                    (current_tile.x, current_tile.y - 1)
+                    (current_tile.x, current_tile.y + 1)
+                ]:
+                    current_tile = None
+                    if neighbor not in traversed and self.tile_grid[neighbor.x][neighbor.y] == 'Path':
+                        traversed.add(neighbor)
+                        if current_tile == None: current_tile = neighbor
+                        else: raise Exception('Branching detected in mercenary path')
+                
+                # Record the next tile
+                computed_path.append(current_tile) 
+        else:
+            return None
