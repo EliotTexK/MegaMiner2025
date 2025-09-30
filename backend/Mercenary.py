cec6e8680a8b3be5d1d76976ca13bf6bdeb16407
mjjm4d <threehalves1@gmail.com>
2025-09-30 18:40:59 -0500

    Implement merc state logic in merc update

--- backend/Mercenary.py
+++ backend/Mercenary.py
@@ -1,12 +1,53 @@
 import Constants
+from GameState import GameState
 
 class Mercenary:
-    def __init__(self, x: int, y: int, team_color: str) -> None:
+    def __init__(self, x: int, y: int, team_color: str, state: str) -> None:
         self.hp = Constants.MERCENARY_INITIAL_HP
         self.x = x
         self.y = y
+        self.state = state
 
         if team_color in ['r','b']:
             self.team = team_color
         else:
             raise Exception("Mercenary team_color must be 'r' or 'b'") # TF2 reference?
+        
+    # Helper function do find what path this merc is on. 
+    # Side effect: passed list item 0 will be path coordinate list, item 1 will be position in list
+    def get_current_path(self, game_state: GameState, data):
+        current_path = []
+        possible_paths = [game_state.mercenary_path_down, 
+                    game_state.mercenary_path_left, game_state.mercenary_path_right,
+                    game_state.mercenary_path_up]
+    
+        # Assumes no overlapping paths. If merc is on any path tile in path, we are on that path
+        for path in possible_paths:
+            if ([self.x, self.y] in path):
+                current_path = path
+
+        data[0] = current_path
+        # position of merc along path (red to blue)
+        data[1] = current_path.index([self.x, self.y])
+
+    # Helper function that returns coordinates of the path tile forward or back from the merc's pos
+    def get_adjacent_path_tile(self, game_state: GameState, delta: int):
+        data = []
+        self.get_current_path(game_state, data)
+        path = data[0]
+        path_pos = data[1]
+
+        # if we are at the end of path, return last tile 
+        # otherwise, return next tiles
+        delta *= 1 if self.team == 'r' else -1
+        return path[min(max(path_pos + delta, 0), len(path))]
+    
+    def set_behind_waiting(self, game_state: GameState):
+        behind_pos = self.get_adjacent_path_tile(game_state, -1)
+        behind_merc: Mercenary = game_state.entity_grid[behind_pos[0]][behind_pos[1]]
+        # base case: we are in the first tile in our path, do not recurse
+        if self.x == behind_pos[0] and self.y == behind_pos[1]:
+            return
+        else:
+            behind_merc.state = 'waiting'
+            behind_merc.set_behind_waiting(game_state)
--- backend/UpdateMercenaries.py
+++ backend/UpdateMercenaries.py
@@ -1,4 +1,58 @@
+import math
+from typing import List
 from GameState import GameState
+from Mercenary import Mercenary
+from Demon import Demon
+from PlayerBase import PlayerBase
 
 def update_mercenaries(game_state: GameState):
-    pass # TODO
+    # Determine all merc states
+    moving: List[Mercenary] = []
+    fighting: List[Mercenary] = []
+    waiting: List[Mercenary] = []
+    set_all_merc_states(game_state, game_state.mercs, moving, fighting, waiting)
+
+    # Move all mercs in moving state
+    for merc in moving:
+        move_merc_single(merc)
+
+    # Apply combat effects for all mercs in fighting state
+    for merc in fighting:
+        do_merc_combat_single(merc)
+
+    pass
+
+# Side effect: All merc objects will have their state set appropiately
+# Side effect: moving, fighting and waiting lists will be populated
+def set_all_merc_states(game_state: GameState, mercs: List[Mercenary], 
+                        moving: List[Mercenary],
+                        fighting: List[Mercenary],
+                        waiting: List[Mercenary]):
+    for merc in mercs:
+        next_tile1 = merc.get_adjacent_path_tile(game_state, 1)
+        next_tile2 = merc.get_adjacent_path_tile(game_state, 2)
+        blocking_entity1 = game_state.entity_grid[next_tile1[0], next_tile1[1]]
+        blocking_entity2 = game_state.entity_grid[next_tile2[0], next_tile2[1]]
+
+        # fighting if rival merc or demon or player base is within 2 spaces
+        for ent in [blocking_entity1, blocking_entity2]:
+            if (type(ent) == type(Demon) or 
+                type(ent) == type(PlayerBase) or 
+                (type(ent) == type(Mercenary) and ent.team != merc.team)):
+                merc.state = 'fighting'
+                # set all mercs behind us to waiting
+                merc.set_behind_waiting(game_state)
+            # if not waiting or fighting, then moving
+            elif merc.state != 'waiting':
+                merc.state = 'moving'
+        
+        # add to correct list
+        if merc.state == 'fighting': fighting.append(merc)
+        if merc.state == 'waiting': waiting.append(merc)
+        if merc.state == 'moving': moving.append(merc)
+
+def move_merc_single(merc: Mercenary):
+    pass
+
+def do_merc_combat_single(merc: Mercenary):
+    pass
